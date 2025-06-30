7331efa81b5ac4638ae33b6e5d15fa5378e4b51a
Ayush <ayushrs7023@gmail.com>
2025-06-30 16:08:55 +0530

    sendAdminEmail

--- app/Http/Controllers/ApiControllers/UserloginController.php
+++ app/Http/Controllers/ApiControllers/UserloginController.php
@@ -866,7 +866,7 @@ class UserloginController extends Controller
             $last_id = DB::table('tbl_doctor')->insertGetId($data_insert);
 
             // $this->sendWelcomeSms($phone, $tempRecord->name, 'doctor', '649e7f1bd6fc0504df28fcc3');
-            $this->sendAdminEmail($tempRecord->toArray(), $last_id, 'doctor');
+            // $this->sendAdminEmail($tempRecord->toArray(), $last_id, 'doctor');
 
             $data = [
                 'name' => $tempRecord->name,
