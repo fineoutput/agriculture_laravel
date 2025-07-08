aba8dbbe796745c9df04c52cda64392b65af39ba
Ayush <ayushrs7023@gmail.com>
2025-07-08 11:19:07 +0530

    messageTest

--- app/Http/Controllers/ApiControllers/SmsController.php
+++ app/Http/Controllers/ApiControllers/SmsController.php
@@ -9,18 +9,17 @@ use App\Services\Msg91Service;
 class SmsController extends Controller
 {
      protected $msg91;
-
-    public function __construct(Msg91Service $msg91)
+ public function __construct(Msg91Service $msg91)
     {
         $this->msg91 = $msg91;
     }
 
-    public function sendTestSMS()
+    public function sendTestSms()
     {
-        $mobile = '9461937396'; // include country code
-        $message = 'Hello! Your test SMS from MSG91 is working.';
+        $mobile = '9461937396'; // Replace with your test number
+        $message = 'Your test OTP is 123456';
 
-        $response = $this->msg91->sendSMS($mobile, $message);
+        $response = $this->msg91->sendTestSMS($mobile, $message);
 
         return response()->json($response);
     }
--- app/Services/Msg91Service.php
+++ app/Services/Msg91Service.php
@@ -6,23 +6,22 @@ use Illuminate\Support\Facades\Http;
 
 class Msg91Service
 {
-    public function sendSMS($mobile, $message)
+    public function sendTestSMS($mobile, $message)
     {
-        $url = 'https://control.msg91.com/api/v2/sendsms'; // or flow endpoint
-
         $response = Http::withHeaders([
-            'authkey' => config('constants.auth_key'),
+            'authkey' => config('constants.MSG91_AUTH_KEY'),
             'Content-Type' => 'application/json',
-        ])->post($url, [
-            'sender' => config('constants.sender_id'),
-            'route' => config('constants.route'),
-            'country' => config('constants.country'),
+        ])->post('https://control.msg91.com/api/v2/sendsms', [
+            'sender' => config('constants.MSG91_SENDER_ID'),
+            'route' => config('constants.MSG91_ROUTE'),
+            'country' => config('constants.MSG91_COUNTRY'),
             'sms' => [
                 [
                     'message' => $message,
                     'to' => [$mobile],
-                ],
-            ],
+                    'template_id' => config('constants.MSG91_LOGIN_DLT'), // You can use any valid template here
+                ]
+            ]
         ]);
 
         return $response->json();
