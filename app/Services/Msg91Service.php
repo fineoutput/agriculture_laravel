5848189624832735fae4eef8157cc4419e9bca23
Ayush <ayushrs7023@gmail.com>
2025-07-08 11:28:52 +0530

    contant

--- app/Services/Msg91Service.php
+++ app/Services/Msg91Service.php
@@ -9,17 +9,17 @@ class Msg91Service
     public function sendTestSMS($mobile, $message)
     {
         $response = Http::withHeaders([
-            'authkey' => config('constants.MSG91_AUTH_KEY'),
+            'authkey' => env('MSG91_AUTH_KEY'),
             'Content-Type' => 'application/json',
         ])->post('https://control.msg91.com/api/v2/sendsms', [
-            'sender' => config('constants.MSG91_SENDER_ID'),
-            'route' => config('constants.MSG91_ROUTE'),
-            'country' => config('constants.MSG91_COUNTRY'),
+            'sender' => env('MSG91_SENDER_ID'),
+            'route' => env('MSG91_ROUTE'),
+            'country' => env('MSG91_COUNTRY'),
             'sms' => [
                 [
                     'message' => $message,
                     'to' => [$mobile],
-                    'template_id' => config('constants.MSG91_LOGIN_DLT'), // You can use any valid template here
+                    'template_id' => config('MSG91_LOGIN_DLT'), // You can use any valid template here
                 ]
             ]
         ]);
