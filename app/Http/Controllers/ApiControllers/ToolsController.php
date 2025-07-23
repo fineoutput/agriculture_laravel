89d997383e80b4b7e1818974e1e0b49e3f1cdf18
office <office.fineoutput@gmail.com>
2025-07-23 16:05:48 +0530

    //

--- app/Http/Controllers/ApiControllers/ToolsController.php
+++ app/Http/Controllers/ApiControllers/ToolsController.php
@@ -1276,6 +1276,7 @@ class ToolsController extends Controller
                     'percent' => $percent,
                     'vendor_id' => $pro->added_by,
                     'is_admin' => (int) $pro->is_admin,
+                    'heloo' => 'heloo',
                     'offer' => $pro->offer,
                     'product_cod' => $pro->cod,
                     'is_cod' => $farmer->cod,
