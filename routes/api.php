f52bec82a11d24d0700055f2b23ffe8fb990b92e
office <office.fineoutput@gmail.com>
2025-07-24 20:03:22 +0530

    get compitition

--- routes/api.php
+++ routes/api.php
@@ -465,7 +465,7 @@ Route::get('/go-live-stream', [GoLiveController::class, 'goLive']);
 
 Route::post('update-live-status', [GoLiveController::class, 'updateLiveStatus']);
 
-Route::post('get-competiton', [GetCompetitionController::class, 'getCompetition']);
+Route::get('get-competiton', [GetCompetitionController::class, 'getCompetition']);
 
 
 Route::post('comp-result', [CompetitionResultController::class, 'storeCompResult']);
